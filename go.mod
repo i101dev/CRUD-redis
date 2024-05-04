b69d357b90f4797388c180bba0e548a5acc1a597
Joshua Abraham <block9services@gmail.com>
2024-05-04 10:33:11 +0800

    chi router and middleware added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/i101dev/microservices-NN
 
 go 1.22.1
+
+require github.com/go-chi/chi/v5 v5.0.12 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hellow", basicHandler)
+
 	// `&` indicates that we are taking the memory address, rather than the value
 	server := &http.Server{
 		Addr:    ":5000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	if err := server.ListenAndServe(); err != nil {
+		fmt.Println()
+		fmt.Print("*** >>> Failed to launch server - Error:")
+		fmt.Println()
 		log.Panic(err)
+		fmt.Println()
 	}
 }
 
