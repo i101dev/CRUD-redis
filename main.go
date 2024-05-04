ad4dd28eb282dfe140d876f01af742d7898132d1
Joshua Abraham <block9services@gmail.com>
2024-05-04 11:59:05 +0800

    CRUD functions and architecture

--- main.go
+++ main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/i101dev/microservices-NN/application"
 )
 
 func main() {
-
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-
-	router.Get("/hellow", basicHandler)
-
-	// `&` indicates that we are taking the memory address, rather than the value
-	server := &http.Server{
-		Addr:    ":5000",
-		Handler: router,
+	app := application.New()
+	if err := app.Start(context.TODO()); err != nil {
+		fmt.Println("failed to start app: ", err)
 	}
-
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Println()
-		fmt.Print("*** >>> Failed to launch server - Error:")
-		fmt.Println()
-		log.Panic(err)
-		fmt.Println()
-	}
-}
-
-// Parameters are in reverse order compared to Nodejs
-// `ResponseWriter` equivalent to [express] ->`res/response`
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Say what again"))
 }
